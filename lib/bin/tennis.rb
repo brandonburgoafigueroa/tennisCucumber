41810ca6f867fb9fec98b7686b46a0fd71cfcf91
brandon <brandonburgoafigueroa@gmail.com>
2017-10-25 00:07:18 -0400

    0 iguales

--- lib/bin/tennis.rb
+++ lib/bin/tennis.rb
@@ -1,4 +1,19 @@
 class Tennis
     def iniciarJuego
+        @nombreJugadorA = "A"
+        @nombreJugadorB = "B"
+        @puntosDeJugadorA = 0
+        @puntosDeJugadorB = 0
+        @tablaPuntaje=["0", "15", "30", "40"]
+    end
+    def anotarPunto(nombreDelJugador)
+        if (nombreDelJugador == @nombreJugadorA)
+            @puntosDeJugadorA += 1
+        else
+            @puntosDeJugadorB += 1
+        end
+    end
+    def score()
+        return "0-Iguales"
     end
 end
--- spec/bin/tennis_spec.rb
+++ spec/bin/tennis_spec.rb
@@ -5,4 +5,8 @@ describe Tennis do
         @tennis=Tennis.new
         @tennis.iniciarJuego
     end
+    it "Si comenzo el juego debe devolver el resultado 0-Iguales" do
+        expect(@tennis.score()).to eq "0-Iguales"
+       end
 end
+
